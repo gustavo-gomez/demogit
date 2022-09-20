c58ce5cae60a01c19485be2b6ca8ef7bb9606c29
Gustavo Gomez <gomezf09@gmail.com>
2022-09-19 23:29:43 -0500

    new branch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React prod
         </a>
       </header>
     </div>
