9e251098bc6241f72074eebb3364b05cabed6177
Gustavo Gomez <gomezf09@gmail.com>
2022-09-19 23:07:15 -0500

    demo

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          esta es una demo
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
